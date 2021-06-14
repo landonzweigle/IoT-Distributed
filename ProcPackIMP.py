fdce06e3adc153ee82913d4fb713387ab5a6bd81
Landon Zweigle <lzweigle@chestnut.cs.colostate.edu>
2021-06-14 14:30:23 -0600

    Determine eth type (ipv4 vs ipv6...)

--- ProcPackIMP.py
+++ ProcPackIMP.py
@@ -1,13 +1,17 @@
-#bin/bash/python
+
 import sys, getopt, datetime, time, os
+import dpkt
 import pandas as pds, pyshark as psk
 
+#Written by Landon Zweigle
 
 # logarithm Base (default = 2)
 LOG_BASE = 2 # math.e
 ##sys.setdefaultencoding('utf8')
 entropy = 0
 leng = 0
+
+capDir = "./Captures/"
 
 
 ##This method has two parts
@@ -20,6 +24,9 @@ def Collect():
 	inFile =""
 	outFile =""
 	inter =""
+
+	if(len(sys.argv) != 2):
+		raise Excpetion("One (or more) arguments must be specified. The first arguement referse to the device being processed (for accessing arg0.pcap/csv)")
 
 	try:
 		try: ##gather pcap file name and storage file name
@@ -34,68 +41,116 @@ def Collect():
 			else:
 				print ("input error!")
 				sys.exit(1)
-		
-		i=0
-		while True:
-##			fileName = time.strftime('IOTpcap_%b-%d-%Y.' + str(i))
-##			inFile = fileName + '.pcap'
-##			os.system('touch ' + inFile)
-##			print('Capture file is [' + inFile + ']')
 
-##			storageName = fileName + '.csv'
-			storageName=sys.argv[1]
-##			i=i+1
-##			print('Storage File is [' + storageName + ']')
-##			print('--------------------------')
-##			print('Starting Packet Header Parse...')
-			
-##			os.system('touch ' + storageName)
-##			os.system('tshark -r ' + inFile + ' -T fields -e frame.number -e frame.protocols -e frame.len -e frame.packet_flags -e frame.time -e frame.time_relative -e eth.src -e eth.dst -e ip.src -e ip.dst -e ip.version -e ip.proto -e ip.ttl -e ip.hdr_len -e ip.len -e ip.id -e ip.flags -e ip.frag_offset -e ip.checksum -e tcp.seq -e tcp.ack -e tcp.srcport -e tcp.dstport -e tcp.hdr_len -e tcp.len -e tcp.flags -e tcp.options.timestamp.tsval -e tcp.options.timestamp.tsecr -e tcp.checksum -e tcp.window_size_value -e http.request.version -E header=y -E separator=, -E quote=d -E occurrence=f > ' + storageName)
-			
-			print('Header Parse Finished...')
-			print('--------------------------')
-			print('Calculating Packet Entropy')
-			# open csv file and add "payload entropy" and "payload length" to columns
-			csvFile = open(storageName,'r')
-			csvR = csv.reader(csvFile)
-			data = []
-			try:
-				row0 = csvR.next()
-				row0.append('Payload Entropy')
-				row0.append('Payload Length')
-				data.append(row0)
-			except StopIteration:
-				print ('No Rows')
-			except:
-				print ('Something else went wrong')
 
-			# make pcapObject to parse the pcap file
-			p = pcap.pcapObject()
-			p.open_offline(inFile)
-			try:
-				for item in csvR:
-					p.dispatch(1, analysepacket)
-					item.append(entropy)
-					item.append(leng)
-					data.append(item)
+		inFile = capDir + sys.argv[1] + '.pcap'
+		storageName = capDir + sys.argv[1] + '.csv'
+		pcap = psk.FileCapture(inFile)
 
-			except KeyboardInterrupt:
-				print ('interrupt')
-			csvFile = open(storageName, 'w')
-			csvW = csv.writer(csvFile)
-			csvW.writerows(data)
-			print('Finished Calcualting Packet Entropy')
-			print('--------------------------')
-			print('Sending Data to Storage')
-			print('--------------------------')
-			##os.system('scp ./' + storageName + ' ./' + inFile + ' jordantp@tokyo.cs.colostate.edu:/s/fir/e/nobackup/nuclear_iot/IOT')
-			print('Finished Sending Data to Storage')
-			print('--------------------------')
-			sys.exit()
+		print('Capture file is [' + inFile + ']')
+		print('Storage File is [' + storageName + ']')
+		print('--------------------------')
+		print('Starting Packet Header Parse...')
+
+		os.system('touch ' + storageName)
+		os.system('tshark -r ' + inFile + ' -T fields -e frame.number -e frame.protocols -e frame.len -e frame.packet_flags -e frame.time -e frame.time_relative -e eth.src -e eth.dst -e ip.src -e ip.dst -e ip.version -e ip.proto -e ip.ttl -e ip.hdr_len -e ip.len -e ip.id -e ip.flags -e ip.frag_offset -e ip.checksum -e tcp.seq -e tcp.ack -e tcp.srcport -e tcp.dstport -e tcp.hdr_len -e tcp.len -e tcp.flags -e tcp.options.timestamp.tsval -e tcp.options.timestamp.tsecr -e tcp.checksum -e tcp.window_size_value -e http.request.version -E header=y -E separator=, -E quote=d -E occurrence=f > ' + storageName)
+
+		print('Header Parse Finished...')
+		print('--------------------------')
+		print('Calculating Packet Entropy')
+
+		#load the parsed header into a DataFrame
+		df = pds.read_csv(storageName, index_col=False)#.dropna(axis=1, how="all") #right now I dont know if I should keep the NA columns.
+
+		#create the out DataFrame (csv)
+		outDF = pds.DataFrame(columns=list(df.columns.values)+["Payload Entropy", "Payload Length"])
+		print(df)
+		print(outDF)
+
+		print()
+
+		#iterate over every packet in the dataframe df. Analyse it, and append the result to the output DataFrame (outdf).
+		res = df.apply(tApply, axis=1, pcap=pcap)
+
+
+		exit()
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+
+		# open csv file and add "payload entropy" and "payload length" to columns
+
+#		data = []
+#		try:
+#			row0 = csvR.next()
+#			row0.append('Payload Entropy')
+#			row0.append('Payload Length')
+#			data.append(row0)
+#		except StopIteration:
+#			print ('No Rows')
+#		except:
+#			print ('Something else went wrong')
+
+		# make pcapObject to parse the pcap file
+#		p = pcap.pcapObject()
+#		p.open_offline(inFile)
+		try:
+			for item in csvR:
+				p.dispatch(1, analysepacket)
+				item.append(entropy)
+				item.append(leng)
+				data.append(item)
+
+		except KeyboardInterrupt:
+			print ('interrupt')
+		csvFile = open(storageName, 'w')
+		csvW = csv.writer(csvFile)
+		csvW.writerows(data)
+
+		print('Finished Calcualting Packet Entropy')
+		print('--------------------------')
+		print('Sending Data to Storage')
+		print('--------------------------')
+		##os.system('scp ./' + storageName + ' ./' + inFile + ' jordantp@tokyo.cs.colostate.edu:/s/fir/e/nobackup/nuclear_iot/IOT')
+		print('Finished Sending Data to Storage')
+		print('--------------------------')
+		sys.exit()
             
 	except KeyboardInterrupt:
 		print("Closing Program...")
 		sys.exit()
+
+def tApply(d, pcap):
+#	if not pcap and not d:
+#		return
+
+	index = d.name
+	packet = pcap[index]
+	print(packet)
+	#check if it is ipv4. This is essentially what extractpayload did.
+	if(int(packet.eth.type,16) == 2048):
+		decodeipv4(packet)
+		
+
+
+	#pktinfos, payload = extractpayload(dpkt.ethernet.Ethernet(data))
+
+	#print(d)
+	print("\nEE\n")
+
+##Some basic processing we did for our paper
+def analysepacket (pktlen, data, timestamp):
+	if not data:
+		return
+
+	pktinfos, payload = extractpayload(dpkt.ethernet.Ethernet(data))
+
+	if pktinfos and payload:
+		global entropy 
+		entropy = Entropy(payload)
+		global leng 
+		leng= len(payload)
 
 def Entropy(data):
    # We determine the frequency of each byte
@@ -163,18 +218,7 @@ def extractpayload(eth):
 	else:
 		return None, None
 
-##Some basic processing we did for our paper
-def analysepacket (pktlen, data, timestamp):
-	if not data:
-		return
 
-	pktinfos, payload = extractpayload(dpkt.ethernet.Ethernet(data))
-
-	if pktinfos and payload:
-		global entropy 
-		entropy = Entropy(payload)
-		global leng 
-		leng= len(payload)
 
 ##This you can change or just change the Collect method to process a pcap file of your choice. Use Linux based python interpreter as it will have the pcap file.
 def main():
