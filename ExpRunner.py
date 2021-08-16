a186f558e6c31d9457f26066a34feea508651343
Landon Zweigle <lzweigle@rams.colostate.edu>
2021-08-16 15:23:25 -0600

    ExpRunner now get the latest Conglomerate CSV (if none is provided)

--- ExpRunner.py
+++ ExpRunner.py
@@ -1,22 +1,57 @@
 import os, sys, MLPipe
 import pandas as pds
 from datetime import datetime
-from Utility import get_unique_filename
+from Utility import *   #get_unique_filename, raise_filePath_DNE, debug
 
-CONG_CSV="./windowParsed/Conglomerate.csv"
+CONG_DIR="./windowParsed"
+CONG_NAME_BASE="Conglomerate"
+CONG_NAME=CONG_NAME_BASE+".csv"
+CONG_CSV=os.path.normpath(CONG_DIR + "/" + CONG_NAME)
 
-def main():
-    cong = cleanDF(pds.read_csv(CONG_CSV, index_col=0))
+def select_cong():
+    files=os.listdir(CONG_DIR)
+    biggestRaw = ""
+    biggest = ""
+
+    print(files)
+
+    for f in files:
+        if(os.path.isdir(f)):
+            debug(f,COLORS.RED)
+            continue
+        if(f.startswith(CONG_NAME_BASE)):
+            fRaw = f.split('.')[0].split(CONG_NAME_BASE)[-1]
+
+            debug("'%s' > '%s' [?]"%(fRaw, biggestRaw),COLORS.BLUE)
+            if(fRaw>biggestRaw):
+                debug("\t-->'%s' > '%s' == True" % (f, biggest),COLORS.GREEN)
+                biggestRaw=fRaw
+                biggest=f
+    return os.path.normpath(CONG_DIR+"/"+biggest)
+
+
+
+
+def main(saveFile, cong=None):
+    if(not cong):
+        cong=select_cong()
+    cong = raise_filePath_DNE(cong)
+
+    debug("Using Conglomerate at %s"%cong, COLORS.GREEN)
+
+    saveFile = raise_filePath_DNE(saveFile)
+
+    congDF = cleanDF(pds.read_csv(cong, index_col=0))
 
     resIndex = []
     resData = []
 
 
-    for uniqueName in cong["Device"].unique():
-        devID = cong[cong["Device"]==uniqueName].index.unique()[0]
+    for uniqueName in congDF["Device"].unique():
+        devID = congDF[congDF["Device"]==uniqueName].index.unique()[0]
         print("DeviceID: %s"%devID)
 
-        df = cong.drop("Device",axis=1)
+        df = congDF.drop("Device",axis=1)
         df.index = df.index.map(lambda x: int(x==devID))
 
         dfMLP = MLPipe.MLP(df)
@@ -28,8 +63,7 @@ def main():
     resDF = pds.DataFrame(data=resData,index=resIndex)
     print(resDF)
     
-    now = datetime.now().strftime("%m-%d-%y")
-    resDF.to_csv(get_unique_filename("results.csv"))
+    resDF.to_csv(saveFile)
     return resDF
 
 def cleanDF(df):
@@ -40,4 +74,8 @@ def cleanDF(df):
     
 
 if __name__=="__main__":
-    main()
+    now = datetime.now().strftime("%m-%d-%y")
+    savePath=get_unique_filename("results[%s].csv"%now)
+    print(savePath)
+    # sys.exit(0)
+    main(savePath)
