be9aace86484930a29c99aa8398b8c65eec26727
Landon Zweigle <lzweigle@rams.colostate.edu>
2021-08-16 14:19:43 -0600

    Added method that verifies path leading to file is valid

--- Utility.py
+++ Utility.py
@@ -3,22 +3,22 @@ from enum import Enum
 from re import finditer
 
 class COLORS(Enum):
-	RED="\033[31m"
-	GREEN="\033[32m"
-	BLUE="\033[34m"
-	WHITE="\033[37m"
+    RED="\033[31m"
+    GREEN="\033[32m"
+    BLUE="\033[34m"
+    WHITE="\033[37m"
 
 def debug(msg="", color=None):
-	if(color != None and not isinstance(color, COLORS)):
-		raise Exception("Debug expected color but %s was given."%type(color))
-	else:
-		if(color!=None):
-			color=color.value
-			end="\033[0m"
-		else:
-			color=""
-			end=""
-	print("%s%s%s"%(color,msg,end))
+    if(color != None and not isinstance(color, COLORS)):
+        raise Exception("Debug expected color but %s was given."%type(color))
+    else:
+        if(color!=None):
+            color=color.value
+            end="\033[0m"
+        else:
+            color=""
+            end=""
+    print("%s%s%s"%(color,msg,end))
 
 # def dict_to_stir()
 
@@ -48,11 +48,28 @@ def starts_with_any(toMatch, startwithArr):
     #this is implicit and doesn't need to be added
     return None
     
+def raise_filePath_DNE(file):
+    if(isinstance(file,str)==False and not file):
+        raise Exception("Argument:'file' is not a valid string.")
+
+    fileSPLT = file.split('/')
+    splitLen = len(fileSPLT)
+
+    if(splitLen==1):
+        return "./%s"%(file)
+    elif(splitLen>1):
+        fileName = fileSPLT[-1]
+        filePath = fileSPLT[:-1]
+        if(os.path.isdir(filePath)==False):
+            raise Exception("File path is not valid.")
+        else
+            return file
+
 #returns name appended to path where name is non-colliding (if file path+name+name.extension exists, return path+name(n)+name.extension where n is the first unique path)
 #path is optional. if not provided return will be CWD+name(n)
 #path must be a valid directory
 def get_unique_filename(name, path='.'):
-    if(isinstance(name,str)==True and not name):
+    if(isinstance(name,str)==False and not name):
         raise Exception("argument:'name' must be a valid (non empty) string.")
 
     if(os.path.isdir(path)==False):
@@ -66,15 +83,15 @@ def get_unique_filename(name, path='.'):
         raise Exception("argument:'name' (provided '%s') must be of the form [*valid os-file characters*].[a-zA-Z]"%name)
 
     baseName = os.path.normpath(path + "/" + name)
-	newPath=baseName
-	tried=0
-	while(os.path.isfile(newPath)):
-		debug("...Path %s exists."%newPath,COLORS.RED)
-		tried+=1
-		newPath=baseName + "(%d)" % tried
-		debug("\tTrying new path %s" % newPath,COLORS.RED)
+    newPath=baseName
+    tried=0
+    while(os.path.isfile(newPath)):
+        debug("...Path %s exists."%newPath,COLORS.RED)
+        tried+=1
+        newPath=baseName + "(%d)" % tried
+        debug("\tTrying new path %s" % newPath,COLORS.RED)
 
 
-	debug("\nexp dir is %s" % newPath, COLORS.GREEN)
-	# os.mkdir(newPath)
-	return newPath
+    debug("\nexp dir is %s" % newPath, COLORS.GREEN)
+    # os.mkdir(newPath)
+    return newPath
